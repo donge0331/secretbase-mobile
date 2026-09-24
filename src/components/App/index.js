a1cd55657899a94df75838c8a9d082bf4a80abd8
donge0331 <donge0331@gmail.com>
2026-09-24 15:53:38 +0200

    same fix again

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import classNames from 'classnames';
 import autobind from 'class-autobind';
 import { DragDropContext } from 'react-dnd';
-import TouchBackend from 'react-dnd-html5-backend';
+import TouchBackend from '@automsoft/react-dnd-touch-backend';
 import download from 'downloadjs';
 import queryString from 'query-string';
 import store from 'store';
